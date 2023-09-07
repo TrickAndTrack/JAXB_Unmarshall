7883f9f2250dede2042e2e8a2a0f3f78bd6ee5dd
TrickAndTarck <nahushzilpilwar@gmail.com>
2023-09-07 17:15:56 -0400

    Annotation package change

--- src/main/java/com/jaxb/model/Employee.java
+++ src/main/java/com/jaxb/model/Employee.java
@@ -1,11 +1,12 @@
 package com.jaxb.model;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+
 
 @Getter
 @Setter
--- src/main/java/com/jaxb/model/Employees.java
+++ src/main/java/com/jaxb/model/Employees.java
@@ -1,14 +1,15 @@
 package com.jaxb.model;
 
+
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "employees")
-@XmlAccessorType (XmlAccessType.FIELD)
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Employees
 {
     @XmlElement(name = "employee")
