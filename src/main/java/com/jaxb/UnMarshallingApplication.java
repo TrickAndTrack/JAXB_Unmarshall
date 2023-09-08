1701118d302be0520aa92a61f863682320d385da
Trick & Track <73180409+TrickAndTrack@users.noreply.github.com>
2023-09-08 14:12:09 -0400

    Update UnMarshallingApplication.java

--- src/main/java/com/jaxb/UnMarshallingApplication.java
+++ src/main/java/com/jaxb/UnMarshallingApplication.java
@@ -35,6 +35,3 @@ public class UnMarshallingApplication implements CommandLineRunner {
 }
 
 
-// https://youtu.be/w3WibDOie1Y?si=YopbwNJzyD8pQ9g3
-// https://github.com/camelya58/spring-jaxb
-// https://youtu.be/j9KypS10ry0?si=gHfrpb2uqYz90ksk
